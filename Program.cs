4fcbe9039eb0ba463c932cebaadb0f27a68b5736
Sergey <tukatuta@mail.ru>
2022-10-11 16:46:52 +0300

    feat: add object installation

--- Program.cs
+++ Program.cs
@@ -1,7 +1,35 @@
-﻿public class OnboardingWay
+﻿using Kysect.MazeGenerator;
+using Kysect.MazeGenerator.MazeGenerators.GrowingTree;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("That's the Way!");
+        int size = 5;
+        IMapGenerator generator = new GrowingTreeGenerator();
+        var maze = new Maze(generator.Generate(size));
+        maze.AddExit();
+
+        for (int i = 0; i < maze.Size; i++)
+        {
+            for (int j = 0; j < maze.Size; j++)
+            {
+                Console.Write(CellToString(maze.Map[i][j]));
+
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+
+        string CellToString(Cells cellType)
+        {
+            return cellType switch
+            {
+                Cells.Wall => "0",
+                Cells.Empty => " ",
+                Cells.Exit => "#",
+                _ => throw new ArgumentOutOfRangeException(nameof(cellType), cellType, null)
+            };
+        }
     }
 }
